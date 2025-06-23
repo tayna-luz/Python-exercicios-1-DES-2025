f9eb1b51472e94f5cd41267f92da8f4a524ba7ba
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-06-23 13:59:42 +0000

    exercicio06.py enviado

--- exercicio06.py
+++ exercicio06.py
@@ -1,3 +1,6 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
+horariodeacesso <= int(input("horario de acesso permitido"))
 
+if 9<= horariodeacesso <= 21:
+    print("a plataforma veste funcionamento perfeitamente")
