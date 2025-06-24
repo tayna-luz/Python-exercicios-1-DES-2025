42bbc4011b145193ac97199d7561ea1b52867df9
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-06-24 13:59:36 +0000

    exercicio03.py enviado

--- exercicio03.py
+++ exercicio03.py
@@ -1,7 +1,7 @@
 # Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
 umidade = int(input("Digite a umidade loca"))
-if umidade >= 70%
-       print("Alerta umidade ultrapassou 70:")
+if umidade >= 70:
+       print("Alerta umidade ultrapassou")
 else:
        print("Alerta temperatura ultrapassou o limite.")
---
+++ exercicio13.py
@@ -0,0 +1,2 @@
+#Crie um programa que receba um ano e diga se ele é bissexto.
+#(Dica: múltiplos de 4, exceto os múltiplos de 100, a menos que também sejam múltiplos de 400)
