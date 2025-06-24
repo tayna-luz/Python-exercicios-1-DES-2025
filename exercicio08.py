b7652bacda2dfa9b85f5dc120552080ef7498147
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-06-24 13:41:13 +0000

    exercicio08.py enviado

--- exercicio08.py
+++ exercicio08.py
@@ -4,4 +4,11 @@
 # De 51 a 150 km: R$ 15,00
 # Acima de 150 km: R$ 25,00
 
+distancia = int(input("digite a distância"))
 
+if distancia <= 50:
+    print("$5,00")
+elif 51 <=distancia <=150:
+     print("$ 15,00")
+elif distancia >150:
+     print("$25,00")       
