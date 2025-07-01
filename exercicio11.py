6eb6f0c777e7b7fa042672c04fd3a66a40455fd0
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-07-01 14:02:47 +0000

    exercicio11.py terminado

--- exercicio11.py
+++ exercicio11.py
@@ -4,3 +4,27 @@
 #Peso normal (18.5 a 24.9)
 #Sobrepeso (25 a 29.9)
 #Obesidade (>= 30)
+
+
+peso = float(input("Digite seu peso em kg (ex: 70.5): "))
+altura = float(input("Digite sua altura em metros (ex: 1.75): "))
+
+   
+if altura <= 0:
+        print("Altura inválida. A altura deve ser maior que zero.")
+else:
+       
+        imc = peso / (altura ** 2)
+
+        print(f"Seu IMC é: {imc:.2f}")
+
+       
+        if imc < 18.5:
+            print("Classificação: Abaixo do peso")
+        elif imc >= 18.5 and imc <= 24.9:
+            print("Classificação: Peso normal")
+        elif imc >= 25 and imc <= 29.9:
+            print("Classificação: Sobrepeso")
+        else: # imc >= 30
+            print("Classificação: Obesidade")
+
--- exercicio18.py
+++ exercicio18.py
@@ -1,4 +1,4 @@
 #Receba duas notas e seus respectivos pesos. Calcule a média ponderada.
 #Fórmula: (nota1 × peso1 + nota2 × peso2) / (peso1 + peso2)
 
-
+    
