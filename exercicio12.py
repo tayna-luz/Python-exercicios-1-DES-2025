ecac0ab6ebba8e6d4db7199663d8ea7bfc918e9c
tayna-luz <tayna.luz.santos@escola.pr.gov.br>
2025-07-08 11:55:42 +0000

    exercicio12.py enviado

--- exercicio12.py
+++ exercicio12.py
@@ -1,2 +1,9 @@
 #Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.
 #Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
+
+senha = input("Digite sua senha: ")
+
+if len(senha) >= 8:
+    print("Senha válida")
+else:
+    print("Senha muito curta")
--- exercicio14.py
+++ exercicio14.py
@@ -3,3 +3,21 @@
 #Compras acima de R$ 500,00 têm 10%
 #Acima de R$ 300,00 têm 5%
 #Menor ou igual a R$ 300,00 não têm desconto
+
+
+valor_compra = float(input("Digite o valor total da compra em R$: "))
+
+   
+if valor_compra > 500:
+        desconto = 0.10 
+        print("Você ganhou 10% de desconto!")
+elif valor_compra > 300: 
+        desconto = 0.05
+        print("Você ganhou 5% de desconto!")
+else: 
+        print("Para compras de R$ 300,00 ou menos, não há desconto.")
+
+valor_final = valor_compra * (1 - desconto)
+print(f"Valor original da compra: R$ {valor_compra:.2f}")
+  
+
